3a0863569abe64abdbd4d27bf561280515cb94e8
Josselin Biot <josselin.biot95@icloud.com>
2026-03-07 12:11:23 +0100

    Initial Commit

---
+++ SnapWake/SnapWakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapWakeApp.swift
+//  SnapWake
+//
+//  Created by Josselin Biot on 07/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct SnapWakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnapWakeTests/SnapWakeTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnapWakeTests.swift
+//  SnapWakeTests
+//
+//  Created by Josselin Biot on 07/03/2026.
+//
+
+import Testing
+@testable import SnapWake
+
+struct SnapWakeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
